e8e018bec6f84fa36dc4b21e0566f0a0290cafdd
Lulu Li <lilulu004@gmail.com>
2016-09-24 14:06:42 +0000

    Add lessons

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
