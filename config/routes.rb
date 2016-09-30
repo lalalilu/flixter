3e5593b2f58705273c1f1b4c9669e917bd513351
Lulu Li <lilulu004@gmail.com>
2016-09-30 15:13:21 +0000

    Section drag and drop

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   namespace :instructor do
     resources :lessons, only: [:update]
+    resources :sections, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
