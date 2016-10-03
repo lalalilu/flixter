b5533f72e99a81b27385bbaf47f5c3ee1c67cf9f
Lulu Li <lilulu004@gmail.com>
2016-10-03 16:54:46 +0000

    Add next lesson button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,13 @@ class Lesson < ActiveRecord::Base
   include RankedModel
 
   ranks :row_order, :with_same => :section_id
+
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    if lesson.blank? && section.next_section
+      section.next_section.lessons.rank(:row_order).first
+    end
+
+    lesson
+  end
 end
